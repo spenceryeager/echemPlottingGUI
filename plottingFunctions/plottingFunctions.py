56200c7188abb364df60225bbd0336d1232c98cc
Spencer Yeager <yeager.spencer@gmail.com>
2021-08-08 00:58:35 -0700

    new files

---
+++ plottingFunctions/plottingFunctions.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as pyplot
+from tkinter import *
+import tkinter.messagebox as mb 
+
+# def plot (figsize, line color, title, axis labels):
+    # makes plot using specified settings
+
+
+# def create
+    # creates dialogue box with plot settings
+    # contains a loop that will iterate until "save" is pressed
